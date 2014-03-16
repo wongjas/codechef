bf5cab721cc85e7b384d656896bcba2991a0f20e
Jason Wong <wongjas@gmail.com>
2014-03-16 10:31:22 -0700

    COINS.py: Unfinished lookup optimization

--- COINS/COINS.py
+++ COINS/COINS.py
@@ -4,6 +4,7 @@ import fileinput
 import math
 import sys
 
+
 # int -> [int]
 def exchange(n):
     first_coin = int(math.floor(n//2))
@@ -11,12 +12,28 @@ def exchange(n):
     third_coin = int(math.floor(n//4))
     return [first_coin, second_coin, third_coin]
 
+calculated = dict([]) # stores the already calculated values
+
+# dict(int:int) -> int or None
+def lookup(key):
+    return calculated.get(key)
+
+# int, int -> None
+def store(key, value):
+    calculated[key] = value
+ 
 # int -> int
 def max_amount(n):
-    coins = exchange(n)
-    if n > sum(coins): #change to lambda function which is faster
+    print >> sys.stderr, n
+    max = lookup(n)
+    if max == None:
+        coins = exchange(n)
+        max = sum(coins) # change to lambda function which is faster
+        store(n, max)
+    if n > max: 
         return n
     else:
+        coins = exchange(n)
         return sum([max_amount(i) for i in coins])
 
 if __name__ == "__main__":
