9a38206a1c5ab17292fd5fc648ca02fa504a3ade
Jason Wong <wongjas@gmail.com>
2014-03-15 20:06:46 -0700

    HORSES.py: Compartmentalization of fxns and got rid of bug where list sorting is not in place

--- HORSES/HORSES.py
+++ HORSES/HORSES.py
@@ -11,23 +11,31 @@ import sys
 def find_diff(int1, int2):
     return abs(int2 - int1)
 
+# [int] -> [int]
+def find_diffs(list):
+    if len(list) == 2:
+        return [find_diff(list[0], list[1])]
+    else:
+        return [find_diff(list[i], list[i+1])
+                for i in range(0, len(list) - 1)]
+
 # [int] -> int
 def min_diff(horseSkills):
     sortedHorses = sorted(horseSkills) # one sort
-    if len(sortedHorses) == 2:
-        return find_diff(sortedHorses[0], sortedHorses[1])
-    else:
-        horseDiffs = [find_diff(sortedHorses[i], sortedHorses[i+1]) for i in range(0, len(sortedHorses) - 2)]
-        sorted(horseDiffs) # second sort
-        return horseDiffs[0]
+    horseDiffs = find_diffs(sortedHorses)
+    return sorted(horseDiffs)[0] # second sort
     
+# str -> [int]
+def str2list_ints(str):
+    return map(int, string.split(str.strip(), " "))  # Give the ints in an array here
+
 # File -> iterator([int])
 def parse_input():
     input = fileinput.input()
     numTestCases = int(input.readline())
     for i in range (0, numTestCases):
         numInts = input.readline()   # Probably do something useful with this later
-        yield map(int, string.split(input.readline().strip(), " "))  # Give the ints in an array here
+        yield str2list_ints(input.readline())  # Give the ints in an array here
 
 
 if __name__ == "__main__":
