6eb7f8c132dbe9020fa45a880ad8a8b5135657f3
Jason Wong <wongjas@gmail.com>
2014-03-15 23:30:18 -0700

    COINS.py: Preliminary solution without optimization

--- COINS/COINS.py
+++ COINS/COINS.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
+import fileinput
+import math
 import sys
 
-def main():
+# int -> [int]
+def exchange(n):
+    first_coin = int(math.floor(n//2))
+    second_coin = int(math.floor(n//3))
+    third_coin = int(math.floor(n//4))
+    return [first_coin, second_coin, third_coin]
 
+# int -> int
+def max_amount(n):
+    coins = exchange(n)
+    if n > sum(coins): #change to lambda function which is faster
+        return n
+    else:
+        return sum([max_amount(i) for i in coins])
 
 if __name__ == "__main__":
-    main()
+    for line in fileinput.input():
+        print >> sys.stdout, max_amount()
