af97b27d1077ae57e4d94ad3105acb715b1093b3
Jason Wong <wongjas@gmail.com>
2014-03-15 23:30:39 -0700

    COINS_test.py: tests the two functions written

---
+++ COINS/COINS_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import unittest
+
+import COINS as coins
+
+class test_COINS(unittest.TestCase):
+    
+    def test_exchange(self):
+        self.assertEquals(coins.exchange(12), [6, 4, 3])
+        self.assertEquals(coins.exchange(24), [12, 8, 6])
+        self.assertEquals(coins.exchange(100), [50, 33, 25])
+
+    def test_max_amount(self):
+        self.assertEquals(coins.max_amount(100), 120)
+        self.assertEquals(coins.max_amount(24), 27)
+        self.assertEquals(coins.max_amount(12), 13)
+
+if __name__ == "__main__":
+    unittest.main()
